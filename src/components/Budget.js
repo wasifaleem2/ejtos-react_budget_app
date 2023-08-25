ac48ae0c6b8a06d5d35048bbb102b5f989903ad1
wasifaleem2 <wasifaleem2@gmail.com>
2023-08-25 14:25:18 -0400

    exam

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,10 +1,17 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
 const Budget = () => {
     const { budget } = useContext(AppContext);
+    const [budgetVal, setBudgetVal] = useState(budget)
+    const budgetValChange = (e) =>{
+        if(e+budgetVal > 20000){
+            alert("the value cannot exceed remaining funds $30")
+        }
+    }
     return (
         <div className='alert alert-secondary'>
-            <span>Budget: £{budget}</span>
+            {/* <span>Budget: £{budget}</span> */}
+            <span>Budget: <input  max="20000" type="number" onChange={budgetValChange} /></span>
         </div>
     );
 };
